39f4a5b8fd2aa32567f519fbe62bd37d01d7e2db
waljqiang <zql185@163.com>
2019-04-19 18:33:39 +0800

    publish

---
+++ test.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . "/vendor/autoload.php";
+use Nova\Mqtt\Mqtt;
+
+$config = [
+	'clientid' => uniqid(),
+	'parameters' => [
+		'address' => '192.168.111.15',
+	    'port' => 1883,
+	    'username' => 'rongbo',
+	    'password' => '111'
+	],
+    'options' => [
+    	'clean' => true,
+    	'will' => NULL,
+    	'mode' => 0,
+    	'keepalive' => 10,
+    	'timeout' => 30,
+    ]
+];
+try{
+	$mqtt = new Mqtt($config['clientid'],$config['parameters'],$config['options']);
+	$rs = $mqtt->publish('abc','111');
+	var_dump($mqtt);
+	var_dump($rs);
+}catch(\Exception $e){
+	var_dump($e);
+}
