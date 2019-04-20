a1d392a32d7567f951b176de1c385d46e5dbfc78
waljqiang <zql185@163.com>
2019-04-20 22:50:01 +0800

    example

--- examples/publish.php
+++ examples/publish.php
@@ -4,11 +4,9 @@ use Nova\Mqtt\Mqtt;
 
 try{
 	$mqtt = new Mqtt($config['clientid'],$config['parameters'],$config['options']);
-	$mqtt->connect();
 	$rs = $mqtt->publish('abc','111');
 	var_dump($mqtt);
 	var_dump($rs);
-	$mqtt->close();
 }catch(\Exception $e){
 	var_dump($e);
 }
