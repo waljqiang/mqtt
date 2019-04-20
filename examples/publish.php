fa301617515c260098cdc4b859dd01bcb898400b
waljqiang <zql185@163.com>
2019-04-20 19:28:33 +0800

    publish

--- examples/publish.php
+++ examples/publish.php
@@ -1,23 +1,7 @@
 <?php
-require_once __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ .'/shared.php';
 use Nova\Mqtt\Mqtt;
 
-$config = [
-	'clientid' => uniqid(),
-	'parameters' => [
-		'address' => '192.168.111.15',
-	    'port' => 1883,
-	    'username' => 'rongbo',
-	    'password' => '111'
-	],
-    'options' => [
-    	'clean' => true,
-    	'will' => NULL,
-    	'mode' => 0,
-    	'keepalive' => 10,
-    	'timeout' => 30,
-    ]
-];
 try{
 	$mqtt = new Mqtt($config['clientid'],$config['parameters'],$config['options']);
 	$rs = $mqtt->publish('abc','111');
---
+++ examples/shared.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$config = [
+	'clientid' => uniqid(),
+	'parameters' => [
+		'address' => '192.168.33.10',
+	    'port' => 1883,
+	    'username' => '',
+	    'password' => ''
+	],
+    'options' => [
+    	'clean' => true,
+    	'will' => NULL,
+    	'mode' => 0,
+    	'keepalive' => 10,
+    	'timeout' => 30,
+    ]
+];
